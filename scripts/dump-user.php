4c93b6a223181c8e8bf840eaa852411496ac2474
Gisle Aas <gisle.aas@it.uib.no>
2014-12-12 12:55:53 +0100

    Fix comment prefix

--- scripts/dump-user.php
+++ scripts/dump-user.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
-/* Sample script that can be invoked with 'site-drush scr dump-user'.
+ * Sample script that can be invoked with 'site-drush scr dump-user'.
  */
 
 print_r(user_load(1));
